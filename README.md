cd86bbb0e78ae9b5f69a78443c553d7b00887e53
juworld <juworld@webtoonscorp.com>
2019-01-27 16:13:23 +0900

    delete notebook file

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 - Conditional Neural Process https://arxiv.org/pdf/1807.01613.pdf
 - Neural Process https://arxiv.org/pdf/1807.01622.pdf
 
-codes are from 
+Running on cpu or gpu
+
+
+
+### codes references
 1. deepmind https://github.com/deepmind/conditional-neural-process
 2. geniki https://github.com/geniki/neural-processes
 3. chrisorm https://chrisorm.github.io/NGP.html
--- np_regression.py
+++ np_regression.py
@@ -1,49 +1,51 @@
 import torch
-import torch.utils.data as data
 import torch.nn as nn
 import torch.optim as optim
 from torch.nn import functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 
-device = torch.device('cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class NP(nn.Module):
-    def __init__(self, in_dim, out_dim, hdim, ddim, z_samples):
+    def __init__(self, hdim, ddim, z_samples):
         super(NP, self).__init__()
-        self.z_dim = out_dim
+        in_dim = 2
+        out_dim = 2
+        self.z_dim = 2
         self.z_samples = z_samples
 
         self.h1 = nn.Linear(in_dim, hdim)
         self.h2 = nn.Linear(hdim, out_dim)
-        #self.h3 = nn.Linear(hdim, out_dim)
 
         self.r_to_z_mean = nn.Linear(in_dim, 1)
         self.r_to_z_std = nn.Linear(in_dim, 1)
 
         self.d1 = nn.Linear(in_dim + 1, ddim)
         self.d2 = nn.Linear(ddim, out_dim)
-        #self.d3 = nn.Linear(ddim, out_dim)
 
         nn.init.normal(self.h1.weight)
         nn.init.normal(self.h2.weight)
-        #nn.init.normal(self.h3.weight)
         nn.init.normal(self.d1.weight)
         nn.init.normal(self.d2.weight)
-        #nn.init.normal(self.d3.weight)
 
-    def data_to_z(self, x, y):
+    def data_to_r(self, x, y):
         x_y = torch.cat([x, y], dim=1)
-        #r_i = self.h3(F.relu(self.h2(F.relu(self.h1(x_y)))))
         r_i = self.h2(F.relu(self.h1(x_y)))
-        r = r_i.mean(dim=0)
-        mean = self.r_to_z_mean(r)
-        std = self.r_to_z_std(r)
-        return mean, F.softplus(std)
 
-    def parametrize(self, mu, std, n):
+        # mean aggregate
+        r = r_i.mean(dim=0)
+        return r
+
+    def r_to_z(self, r):
+        mean = self.r_to_z_mean(r)
+        log_var = self.r_to_z_std(r)
+        return mean, F.softplus(log_var)
+
+    def reparametrize(self, mu, std, n):
         eps = torch.autograd.Variable(std.data.new(n, self.z_dim).normal_())
-        return eps.mul(std).add_(mu)
+        z = eps.mul(std).add_(mu)
+        return z
 
 
     def decoder(self, x_pred, z):
@@ -52,20 +54,22 @@ class NP(nn.Module):
 
         x_z = torch.cat([x_pred, z], dim=-1)
         decode = self.d2(F.sigmoid(self.d1(x_z).squeeze(-1).transpose(0, 1)))
-        mu, logvar = torch.split(decode, 1, dim=-1)
+        mu, logstd = torch.split(decode, 1, dim=-1)
         mu = mu.squeeze(-1)
-        logvar = logvar.squeeze(-1)
-        var = F.softplus(logvar)
-        return mu, var
+        logstd = logstd.squeeze(-1)
+        std = F.softplus(logstd)
+        return mu, std
 
     def forward(self, inputs):
         x_context, y_context, x_target, y_taget = inputs
         x_all = torch.cat([x_context, x_target], dim = 0)
         y_all = torch.cat([y_context, y_taget], dim = 0)
-        z_mean, z_std = self.data_to_z(x_context, y_context)
-        z_mean_all, z_std_all = self.data_to_z(x_all, y_all)
+        r = self.data_to_r(x_context, y_context)
+        z_mean, z_std = self.r_to_z(r)
+        r_all = self.data_to_r(x_all, y_all)
+        z_mean_all, z_std_all = self.r_to_z(r_all)
 
-        zs = self.parametrize(z_mean_all, z_std_all, self.z_samples)
+        zs = self.reparametrize(z_mean_all, z_std_all, self.z_samples)
         mu, std = self.decoder(x_context, zs)
         return mu, std, z_mean_all, z_std_all, z_mean, z_std
 
@@ -84,8 +88,9 @@ def random_split_c_t(x, y, n_context):
     return [x[mask], y[mask], np.delete(x, mask, axis=0), np.delete(y, mask, axis=0)]
 
 def visualize(x, y, x_star, model):
-    z_mu, z_std = model.data_to_z(x, y)
-    zsamples = model.parametrize(z_mu, z_std, 10)
+    r_z = model.data_to_r(x, y)
+    z_mu, z_std = model.r_to_z(r_z)
+    zsamples = model.reparametrize(z_mu, z_std, 3)
     mu, sigma = model.decoder(x_star, zsamples)
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -96,33 +101,35 @@ def visualize(x, y, x_star, model):
             (mu[:, i] + sigma[:, i]).detach().cpu().numpy(), alpha=0.2
         )
         ax.scatter(x.data.cpu().numpy(), y.data.cpu().numpy(), color='b')
-        ax.plot(all_x_np, all_y_np)
-    plt.pause(3)
+        ax.plot(all_x_np, all_y_np, color='b')
+    plt.pause(0.0001)
     fig.canvas.draw()
 
-x_grid = torch.from_numpy(np.arange(-4, 4, 0.1).reshape(-1, 1).astype(np.float32))
-all_x_np = np.arange(-4, 4, .1).reshape(-1, 1).astype(np.float32)
-all_y_np = np.sin(all_x_np)
-# all_y_np = np.exp(np.cos(all_x_np))**3 * 2*np.sin(all_x_np) - np.sin(all_x_np)*np.cos(all_x_np)
+x_grid = torch.from_numpy(np.arange(-5, 5, .1).reshape(-1, 1).astype(np.float32))
 
-model = NP(2, 2, 4, 4, 85).to(device)
+# create dataset
+all_x_np = np.arange(-5, 5, .1).reshape(-1, 1).astype(np.float32)
+# all_y_np = np.sin(all_x_np)
+all_y_np = np.exp(np.cos(all_x_np))**3 * 2*np.sin(all_x_np) - np.sin(all_x_np)*np.cos(all_x_np)
+
+model = NP(6, 6, 20).to(device)
 optimizer =optim.Adam(model.parameters(), lr=0.1)
 
 def train(epochs):
-    training_loss = 0
     for epoch in range(epochs):
         optimizer.zero_grad()
-        inputs = random_split_c_t(all_x_np, all_y_np, np.random.randint(10, 20))
+        inputs = random_split_c_t(all_x_np, all_y_np, np.random.randint(1, 5))
         for i in range(len(inputs)):
             inputs[i] = torch.from_numpy(inputs[i]).to(device)
         mu, std, z_mean_all, z_std_all, z_mean, z_std = model(inputs)
         loss = -log_likelihood(mu, std, inputs[1]) + KLD_gaussian(z_mean_all, z_std_all, z_mean, z_std)
         loss.backward()
-        training_loss += loss.item()
+        training_loss = loss.item()
         optimizer.step()
         print('epoch: {} loss: {}'.format(epoch, training_loss/200))
+
         if epoch % 100 == 0:
             visualize(inputs[0], inputs[1],
-                      torch.from_numpy(np.arange(-4, 4, .1).reshape(-1, 1).astype(np.float32)), model)
+                      torch.from_numpy(np.arange(-5, 5, .1).reshape(-1, 1).astype(np.float32)), model)
 
 train(90000)
